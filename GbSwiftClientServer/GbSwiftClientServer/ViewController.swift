431faea9d7d44eefb7ab74c2598edc77b90073f5
catostropher <catostropher@gmail.com>
2021-08-15 21:27:30 +0700

    Initial commit

---
+++ GbSwiftClientServer/GbSwiftClientServer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GbSwiftClientServer
+//
+//  Created by nanoman on 15.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
